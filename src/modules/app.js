abc2fc56a01191123d428259f81bb128a17e9479
deysan <ideysan@gmail.com>
2021-11-14 22:45:20 +0200

    Update app module

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,14 +1,26 @@
 import { DonateForm } from '../modules/donate-form';
+import { DonateList } from '../modules/donate-list';
 
 export class App {
-    #donat
+    #donate
+    #donateList
 
     constructor() {
-        this.#donat = new DonateForm();
+        this.#donate = new DonateForm();
+        this.#donateList = new DonateList(mockDonates);
     }
-    run() {
-        const donatBlock = this.#donat.render();
 
-        document.body.append(donatBlock);
+    run() {
+        const donatBlock = this.#donate.render();
+        const donatListBlock = this.#donateList.render();
+
+        document.body.append(donatBlock, donatListBlock);
     }
 }
+
+const mockDonates = [
+    { amount: 4, date: new Date() },
+    { amount: 20, date: new Date() },
+    { amount: 3, date: new Date() },
+    { amount: 1, date: new Date() },
+];
